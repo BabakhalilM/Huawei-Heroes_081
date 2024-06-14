3da054a4a3f56b43173fb0c3d84ba4a9c7b57afe
OnkarChikane <onkarchikane00@gmail.com>
2024-06-14 22:16:56 +0530

    code Bestseller

---
+++ my-app/src/componentsft30_312/HeaderImage.jsx
@@ -0,0 +1,14 @@
+// src/components/HeaderImage.js
+import React from 'react';
+
+import './HeaderImage.css'; // Create this CSS file for styling
+
+const HeaderImage = () => {
+  return (
+    <div className="header-image">
+      <img src="https://haanready.com/cdn/shop/collections/FotosSubscription_Mesa_de_trabajo_1_copia_4.jpg?v=1711041291&width=1920" alt="Header" />
+    </div>
+  );
+};
+
+export default HeaderImage;
