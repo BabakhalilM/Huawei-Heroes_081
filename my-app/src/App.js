e71084d9e1151fee0b6b484ff9cf71abb37f69c7
jayant <jayantjagtap001@gmail.com>
2024-06-16 17:24:53 +0900

    adding admin panel

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,15 +3,17 @@ import Aboutpage from "./Pages/Aboutpage";
 import { RefillStation } from "./Pages/RefillStation";
 import { Sets } from "./Pages/Sets";
 import { Shop } from "./Pages/Shop";
+import Adminpannel from "./AdminPaanel/Adminpannel";
 
 export const App = () => {
   return (
     <>
       <div style={{ display: "flex" }}>
         {/* <Aboutpage /> */}
-        {/* <RefillStation /> */}
+        <RefillStation />
         {/* <Sets /> */}
-        <Shop/>
+        {/* <Shop/> */}
+        {/* <Adminpannel/> */}
       </div>
     </>
   );
